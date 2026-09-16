4f7ee31bada5c84982ed91ff5bb9aaa66f723626
CarlosSalinas25 <20210290@ricaldone.edu.sv>
2026-09-16 08:14:10 -0600

    feat:Creacion de variables de entorno y otra modificacion en la estructura de carpetas

---
+++ prueba-firebase-Andrea-Diego/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEQVenHnZixlh_HSYcVz2yPcf25QZHr8w",
+  authDomain: "fir-evaluacion-1d973.firebaseapp.com",
+  projectId: "fir-evaluacion-1d973",
+  storageBucket: "fir-evaluacion-1d973.firebasestorage.app",
+  messagingSenderId: "737849930714",
+  appId: "1:737849930714:web:ef6459cd2ff4f20732c6e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
